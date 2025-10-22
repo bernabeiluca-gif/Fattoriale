ceb467a8a8bedcaeb72487d5e11b8e32ef35a898
bernabeiluca-gif <bernabei.luca@franchettisalviani.net>
2025-10-23 01:13:27 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Fattoriale;
+
+class Program
+{
+    static bool Fattoriale(int n)
+    {
+        if(n%2==0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    static void Main(string[] args)
+    {
+        int somma = 0;
+        for(int i=1; i<=10; i++)
+        {
+            if(Fattoriale==true)
+            {
+                somma = somma + Fattoriale();
+            }
+        }
+        Console.WriteLine("La somma dei fattoriali dei numeri pari fino a 10 è " + somma);
+    }
+   
+    
+}
